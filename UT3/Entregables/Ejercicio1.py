eb7ee68e34169377c202f58fad494a3b530d26c5
angelTormos <angtrogua2@alu.edu.gva.es>
2025-11-04 09:47:58 +0100

    04/11/2025

--- UT3/Entregables/Ejercicio1.py
+++ UT3/Entregables/Ejercicio1.py
@@ -1,7 +1,13 @@
 import shutil
 import sys
 
-umbral = int(sys.argv[1])
+
+if len (sys.argv) == 2:
+    umbral = int(sys.argv[1])
+else:
+    print("Introduzca un unico parametro. Usando umbral por defecto 85")
+    umbral = 85
+
 uso = shutil.disk_usage("/")
 porcentaje = uso.used / uso.total * 100
 
