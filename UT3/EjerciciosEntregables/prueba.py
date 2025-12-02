d81f928634fdb8ce6640da9608a6966772d847b6
angelTormos <angtrogua2@alu.edu.gva.es>
2025-12-02 09:55:12 +0100

    02/12/2025

---
+++ UT3/EjerciciosEntregables/prueba.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+ip_base = input("Introduce una IP ")
+rango_ini = int(input("Introduce el inicio del rango "))
+rango_fin = int(input("Introduce el fin del rango "))
+max_host = int(input("Introduce el maximo de hosts "))
+
+for i in range(rango_ini, rango_fin + 1):
+    print(f"{ip_base}{i}.1")
+    for f in range(2, max_host + 1):
+        if f % 5 == 0:
+            continue
+        else:
+            print(f"{ip_base}{i}.{f}")
